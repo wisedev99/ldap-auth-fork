20ef28daff30b9996ce9537a3eb2d084d36d1fff
Wise Dev <86822394+wisedev99@users.noreply.github.com>
2023-08-30 17:45:24 +0500

    updateOrCreate user in local database

--- index.js
+++ index.js
@@ -55,6 +55,32 @@ const authenticate = (username, password, done) => {
         } else if (user) {
             done(null, user); // Authentication successful, pass the user object
             console.log('Authentication failed', user);
+            // insert or update the user object into the database and pass the user object into the middleware function
+            pool.query('SELECT * FROM users WHERE username = ?', [username], (err, results) => {
+                if (err) {
+                    done(err);
+                } else {
+                    if (results.length > 0) {
+                        // User exists, update their information in the database
+                        pool.query('UPDATE users SET password = ? WHERE username = ?', [password, username], (err) => {
+                            if (err) {
+                                done(err);
+                            } else {
+                                done(null, user);
+                            }
+                        });
+                    } else {
+                        // User doesn't exist, add them to the database
+                        pool.query('INSERT INTO users (username, password) VALUES (?, ?)', [username, password], (err) => {
+                            if (err) {
+                                done(err);
+                            } else {
+                                done(null, user);
+                            }
+                        });
+                    }
+                }
+            });
 
         } else {
             done(new Error('Invalid username or password'));
